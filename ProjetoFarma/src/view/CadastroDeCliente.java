bbd3092c87aecc8d63124ce0a6ac193a384b4141
vitor <vitor@DESKTOP-442JIE5>
2024-06-12 16:05:23 -0300

    Pesquisa Nome em Tempo Real

--- ProjetoFarma/src/view/CadastroDeCliente.java
+++ ProjetoFarma/src/view/CadastroDeCliente.java
@@ -690,7 +690,7 @@ new HashMap<>(): Isso está criando uma nova instância de um HashMap. O operado
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windos".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
